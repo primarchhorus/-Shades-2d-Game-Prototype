eb26e111199e8f90c1d81f899ba743e5b392b17f
primarchhorus <benjamingarner@Benjamins-MacBook-Air.local>
2014-08-31 22:00:13 +1000

    Unity project of "Shades" prototype
    This is the collaborative project of the unity 2d platformer “Shades”
    (working title). By Benjamin Garner and Tristan Tait.

---
+++ controllerTest/Assets/scripts/ParallaxController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ParallaxController : MonoBehaviour
+{
+	public GameObject[] clouds;
+	public GameObject[] mountainBack;
+	public GameObject[] mountainMed;
+	public GameObject[] treeBack;
+	public GameObject[] foreRock;
+	public GameObject[] playerLevel;
+	public GameObject[] treeFore;
+	public GameObject[] playerRock;
+	public float cloudLayerSpeedModifier;
+	public float mountainBackLayerSpeedModifier;
+	public float mountainMedLayerSpeedModifier;
+	public float treeBackLayerSpeedModifier;
+	public float foreRockLayerSpeedModifier;
+	public float treeForeLayerSpeedModifier;
+	public float playerLevelLayerSpeedModifier;
+	public float playerRockLayerSpeedModifier;
+	public Camera myCamera;
+	private Vector3 lastCamPos;
+
+	void Start()
+	{
+		lastCamPos = myCamera.transform.position;
+	}
+	void Update()
+	{
+		Vector3 currCamPos = myCamera.transform.position;
+		float xPosDiff = lastCamPos.x - currCamPos.x;
+		adjustParallaxPositionsForArray(clouds,
+		                                cloudLayerSpeedModifier, xPosDiff);
+		adjustParallaxPositionsForArray(mountainBack,
+		                                mountainBackLayerSpeedModifier, xPosDiff);
+		adjustParallaxPositionsForArray(mountainMed,
+		                                mountainMedLayerSpeedModifier, xPosDiff);
+		adjustParallaxPositionsForArray(treeBack,
+		                                treeBackLayerSpeedModifier, xPosDiff);
+		adjustParallaxPositionsForArray(foreRock,
+		                                foreRockLayerSpeedModifier, xPosDiff);
+		adjustParallaxPositionsForArray(treeFore,
+		                                treeForeLayerSpeedModifier, xPosDiff);
+		adjustParallaxPositionsForArray(playerLevel,
+		                                playerLevelLayerSpeedModifier, xPosDiff);
+		adjustParallaxPositionsForArray(playerRock,
+		                                playerRockLayerSpeedModifier, xPosDiff);
+		lastCamPos = myCamera.transform.position;
+	}
+	void adjustParallaxPositionsForArray(GameObject[]
+	                                     layerArray, float layerSpeedModifier, float xPosDiff)
+	{
+		for(int i = 0; i < layerArray.Length; i++)
+		{
+			Vector3 objPos = 
+				layerArray[i].transform.position;
+			objPos.x += xPosDiff * layerSpeedModifier;
+			layerArray[i].transform.position = objPos;
+		}
+	}
+}
---
+++ controllerTest/Assets/scripts/scroll.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+	public float scrollSpeed;
+	private Vector2 savedOffset;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	public void scrollParrallaxRight() {
+
+		float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2 (x, 0);
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+
+		}
+
+	
+	public void scrollParrallaxLeft() {
+		float x = Mathf.Repeat (Time.time * (scrollSpeed*1), 1);
+		Vector2 offset = new Vector2 (x*-1, 0);
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	
+		
+	}
+}
